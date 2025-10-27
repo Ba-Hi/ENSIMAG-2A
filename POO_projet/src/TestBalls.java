fac69c9278813d4f08ce95304ebb115872c0371a
Hiba Hanini <hiba.hanini@grenoble-inp.org>
2025-10-27 21:07:14 +0100

    TP java

---
+++ POO_projet/src/TestBalls.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class BallsTest {
+
+    @org.junit.jupiter.api.Test
+    void translate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void reInit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
